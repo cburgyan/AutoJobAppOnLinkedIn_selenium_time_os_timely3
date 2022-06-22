a6ada1ea23c40a29eba1f003ef0f3a161b310a22
cburgyan <cburgyan@gmail.com>
2022-06-21 22:32:00 -0500

    add save job listing, add follow company, and add those actions for all results in search

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import selenium.common.exceptions
+
 import timely3
 import time
 from selenium import webdriver
@@ -29,13 +31,10 @@ driver.get(URL)
 #Create selenim.webdriver.remote.webelement.WebElement
 time.sleep(2)
 sign_we = driver.find_element(by=By.CLASS_NAME, value="cta-modal__primary-btn")
-# user_we = driver.find_element(by=By.ID, value="email-or-phone")
-# password_we = driver.find_element(by=By.ID, value="password")
-# agree_we = driver.find_element(by=By.ID, value="join-form-submit")
 
 
 #Program Flow
-#--Login
+#--Click Sign in
 sign_we.click()
 
 
@@ -51,31 +50,49 @@ password_we.send_keys(os.environ.get("PASS"))
 agree_we.click()
 
 
-#Save Company
-#--Create WebElement for Save
+#--Get Number of Search Results
 time.sleep(5)
-save_we = driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[3]/div[3]/div[2]/div/section[2]/div/div/div[1]/div/div[1]/div/div[1]/div[1]/div[3]/div/button')
-save_we.click()
+number_of_job_listings_we = driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[3]/div[3]/div[2]/div/section[1]/div/header/div[1]/small')
+num_of_job_listings = int(number_of_job_listings_we.text.split(" ")[0])
+print(num_of_job_listings)
+for index in range(1, num_of_job_listings + 1):
+    #Select Next Job Listing
+    time.sleep(2)
+    job_title2_we = driver.find_element(by=By.XPATH, value=f'/html/body/div[5]/div[3]/div[3]/div[2]/div/section[1]/div/div/ul/li[{index}]')
+    job_title2_we.click()
 
 
-#Select Company LinkedIn Page
-time.sleep(2)
-company_we = driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[3]/div[3]/div[2]/div/section[1]/div/div/ul/li[1]/div/div[1]/div[1]/div[2]/div[2]/a')
-company_we.click()
+    #Save Company
+    time.sleep(5)
+    save_we = driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[3]/div[3]/div[2]/div/section[2]/div/div/div[1]/div/div[1]/div/div[1]/div[1]/div[3]/div/button')
+    if save_we.text.split("\n")[0] != "Saved":
+        save_we.click()
+    print(save_we.text.split("\n")[0])
+
+    #Select Company Link
+    time.sleep(2)
+    company2_we = driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[3]/div[3]/div[2]/div/section[2]/div/div/div[1]/div/div[1]/div/div[1]/div[1]/div[1]/span[1]/span[1]/a')
+    company_name = company2_we.text
+    company2_we.click()
 
 
-#--Follow Company
-time.sleep(3)
-# follow_we = driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[3]/div/div[2]/div/div[2]/main/div[1]/section/div/div[2]/div[1]/div[3]/div[1]/div[1]/button')
-# follow_we.click()
+    #Follow Company
+    time.sleep(3)
+    try:
+        follow_we = driver.find_element(by=By.XPATH, value='/html/body/div[5]/div[3]/div/div[2]/div/div[2]/main/div[1]/section/div/div[2]/div[1]/div[3]/div[1]/div[1]/button')
+        if follow_we.text != "Following":
+            follow_we.click()
+    except selenium.common.exceptions.NoSuchElementException as error_message:
+        print(f'Could NOT find a follow button for the company, "{company_name}".')
 
 
-#Return to Previous Page
-time.sleep(2)
-# webdriver.ActionChains(driver).send_keys(Keys.ALT, Keys.LEFT, Keys.ALT).perform()
-driver.back()
+    #Go Back to Search Results Page
+    time.sleep(4)
+    driver.back()
+
 
 #Quit Driver
-#driver.quit()
+time.sleep(5)
+driver.quit()
 
 
