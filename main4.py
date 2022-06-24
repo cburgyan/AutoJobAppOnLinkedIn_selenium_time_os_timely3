aacbc6bcd3cde34b9392a5efcac79957f622df3c
cburgyan <cburgyan@gmail.com>
2022-06-23 19:27:01 -0500

    create main4.py

---
+++ main4.py
@@ -0,0 +1,8 @@
+import timely3
+
+
+coding_timer = timely3.CodingTimer("time_main.txt")
+# coding_timer.start_time()
+# coding_timer.pause_time()
+# coding_timer.unpause_time()
+# coding_timer.stop_time()
